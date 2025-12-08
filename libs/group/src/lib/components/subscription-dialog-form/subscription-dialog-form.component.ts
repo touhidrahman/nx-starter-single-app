25351faea2d91e16cd707a65e07786a0c3e6e562
Touhid Rahman <hello@touhidrahman.me>
2025-12-08 07:37:11 -0500

    refactor: remove group-related components and services
    - Deleted PermissionsContentComponent and its associated styles and template.
    - Removed SubscriptionDialogFormComponent and its styles and template.
    - Eliminated GroupApiService, GroupFormService, GroupListStateService, GroupStateService, InvitationApiService, InvitationFormService, InvitationStateService, and related models.
    - Cleaned up tsconfig files and removed group references from tsconfig.base.json.

--- libs/group/src/lib/components/subscription-dialog-form/subscription-dialog-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'app-subscription-dialog-form',
-    imports: [],
-    templateUrl: './subscription-dialog-form.component.html',
-    styleUrl: './subscription-dialog-form.component.scss',
-})
-export class SubscriptionDialogFormComponent {}
--- libs/group/src/lib/member-request.model.ts
+++
@@ -1,8 +0,0 @@
-export interface MemberRegisterRequest {
-    id?: string
-    firstName?: string
-    lastName?: string
-    email?: string
-    password?: string
-    creatorId?: object
-}
